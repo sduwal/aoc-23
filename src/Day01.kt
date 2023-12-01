1c335d4354c2fefe3b00e53f0671a94320a27d37
sarose012 <sarose012@gmail.com>
2023-12-01 07:14:39 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,63 @@
+val mapNumberToDigit =
+        mapOf(
+                "one" to 1,
+                "two" to 2,
+                "three" to 3,
+                "four" to 4,
+                "five" to 5,
+                "six" to 6,
+                "seven" to 7,
+                "eight" to 8,
+                "nine" to 9,
+                "zero" to 0
+        )
+
+fun findNumber(string: String, shouldReturnOnFirstFind: Boolean = true): Int {
+    var number = 0
+    var tempString = string
+    while (!tempString.isEmpty()) {
+        if (tempString.first().isDigit()) {
+            number = tempString.first().digitToInt()
+            if (shouldReturnOnFirstFind) return number
+        } else {
+            for ((key, value) in mapNumberToDigit) {
+                if (tempString.startsWith(key)) {
+                    number = value
+                    if (shouldReturnOnFirstFind) return number
+                }
+            }
+        }
+        tempString = tempString.drop(1)
+    }
+
+    return number
+}
+
+fun getNumber(string: String): Int {
+    var first = findNumber(string, true)
+    var last = findNumber(string, false)
+
+    return first * 10 + last
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (line in input) {
+            val list = line.toCharArray().filter { it.isDigit() }.map { it.digitToInt() }
+            sum += 10 * list.first() + list.last()
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        var sum = 0
+        for (line in input) {
+            sum += getNumber(line)
+        }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        return sum
+    }
 
     val input = readInput("Day01")
     part1(input).println()
